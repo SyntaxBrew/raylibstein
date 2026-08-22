07c25986725ba3a3c42dce99278db70134b3bc6f
SyntaxBrew <SyntaxBrew@outlook.com>
2026-08-21 20:00:05 -0700

    Create raylib boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "raylib.h"
+
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
 
 int main() {
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Liminalstein");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
     std::cout << "Hello, Backrooms!\n";
     return 0;
 }
